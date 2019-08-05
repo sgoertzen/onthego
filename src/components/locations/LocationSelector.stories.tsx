01b3c62b25569cfe6f992d6a3b9a6f28d6c97d53
Shawn Goertzen <sgoertzen@gmail.com>
2019-08-05 12:13:09 -0400

    Reorg (#46)
    * Folders for locations, posts, and common components.
    
    * Better location page mobile experience
    
    * Location bar swtiches to dropdown on mobile
    
    * Rename Menubar to LocationSelector

---
+++ src/components/locations/LocationSelector.stories.tsx
@@ -0,0 +1,44 @@
+/// <reference types="storybook__react" />
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import LocationSelector from './LocationSelector';
+import { TravelLocation } from '../../classes/TravelLocation';
+import { GeoPoint } from '../../classes/GeoPoint';
+import { TimeStamp } from '../../classes/TimeStamp';
+
+storiesOf('locations/Location Selector', module)
+    .add('no locations', () => <LocationSelector locs={[]} onLocChange={() => { }} />)
+    .add('locations', () => {
+        const locs: TravelLocation[] = []
+        locs.push(new TravelLocation("1", "Alpha", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(10000)))
+        locs.push(new TravelLocation("2", "Beta", new GeoPoint(35, -106), new TimeStamp(10000), new TimeStamp(2562367819120)))
+        locs.push(new TravelLocation("3", "Charlie", new GeoPoint(38, -90), new TimeStamp(2562367819120), new TimeStamp(3562367819120)))
+        return <LocationSelector locs={locs} onLocChange={(id: string) => { alert('switch to loc: ' + id) }} />
+    })
+    .add('2nd selected', () => {
+        const locs: TravelLocation[] = []
+        locs.push(new TravelLocation("1", "Alpha", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(10000)))
+        locs.push(new TravelLocation("2", "Beta", new GeoPoint(35, -106), new TimeStamp(10000), new TimeStamp(2562367819120)))
+        locs.push(new TravelLocation("3", "Charlie", new GeoPoint(38, -90), new TimeStamp(2562367819120), new TimeStamp(3562367819120)))
+        return <LocationSelector locs={locs} onLocChange={(id: string) => { alert('switch to loc: ' + id) }} selectedLocation={locs[1]} />
+    })
+    .add('16 locations', () => {
+        const locs: TravelLocation[] = []
+        locs.push(new TravelLocation("1", "Rockville", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("2", "San Cartli", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("3", "Loa", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("4", "Mt. Donsit", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("5", "Derrali", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("6", "Vash", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("7", "Fetualis", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("8", "Quenta", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("9", "Maliny", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("10", "Erogden", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("11", "Alipnow", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("12", "Crotlohon", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("13", "Alyk", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("14", "Ecyrb", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("15", "Yecats", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        locs.push(new TravelLocation("16", "Nwahs", new GeoPoint(45, -93), new TimeStamp(0), new TimeStamp(0)))
+        return <LocationSelector locs={locs} onLocChange={(id: string) => { alert('switch to loc: ' + id) }} selectedLocation={locs[1]} />
+    })
---
+++ src/components/locations/LocationSelector.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { ITravelLocation } from '../../classes/TravelLocation';
+import { ILocChangeCallback } from '../../classes/ILocChangeCallback';
+import Tabs from '@material-ui/core/Tabs';
+import { Tab, AppBar, Select, MenuItem } from '@material-ui/core';
+import './LocationSelector.css'
+
+interface buttonProps {
+    locs: ITravelLocation[]
+    selectedLocation?: ITravelLocation
+    onLocChange: ILocChangeCallback
+}
+
+class LocationSelector extends React.Component {
+
+    public props: buttonProps
+
+    constructor(props: buttonProps) {
+        super(props);
+        this.props = props;
+        this.tabChange = this.tabChange.bind(this);
+        this.optionChange = this.optionChange.bind(this);
+    }
+
+    tabChange(event: React.ChangeEvent<{}>, newValue: number) {
+        this.props.onLocChange(this.props.locs[newValue].id)
+    }
+
+    optionChange(event: React.ChangeEvent<{ name?: string | undefined; value: unknown; }>) {
+        for (let loc of this.props.locs) {
+            if (loc.name === event.target.value) {
+                this.props.onLocChange(loc.id)
+                return;
+            }
+        }
+    }
+
+    render() {
+        let counter = 0
+        let selectedIndex = 0;
+        let selectedValue = ""
+        // TODO: Fix this when I am back online and can look up the type!
+        let buttons: any[] = []
+        let options: any[] = []
+        for (let loc of this.props.locs) {
+            buttons.push(<Tab label={loc.name} key={counter} />)
+            options.push(<MenuItem value={loc.name} key={counter}>{loc.name}</MenuItem>)
+            if (loc === this.props.selectedLocation) {
+                selectedIndex = counter
+                selectedValue = loc.name
+            }
+            counter++
+        }
+        if (this.props.locs.length === 0) {
+            buttons.push(
+                <Tab label="Loading locations" disabled={true} key="nolocs"></Tab>
+            )
+            options.push(
+                <MenuItem key="nolocs" disabled={true}>Loading Locations</MenuItem>
+            )
+        }
+        return (
+            <div>
+                <div className="LocationSelectorDropdown">
+                    <Select value={selectedValue} onChange={this.optionChange} fullWidth>
+                        {options}
+                    </Select>
+                </div>
+                <div className="LocationSelectorTabs">
+                    <AppBar position="static">
+                        <Tabs value={selectedIndex} onChange={this.tabChange}>
+                            {buttons}
+                        </Tabs>
+                    </AppBar>
+                </div>
+            </div>
+        );
+    }
+}
+export default LocationSelector;
