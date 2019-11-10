55d9f1732b2a8fad0bd52b6118bac9f6b7fbda32
Shawn Goertzen <sgoertzen@gmail.com>
2019-11-10 19:39:06 +0200

    Fixed unable to switch to a post from another post

--- src/components/posts/PostPage.tsx
+++ src/components/posts/PostPage.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { IPost } from '../../classes/Post';
-import PostMenu from './PostMenu';
-import PostHeader from './PostHeader';
-import PostMedia from './PostMedia';
-import "firebase/firestore";
-import { IHistoryProps } from '../../classes/IHistoryProps';
-import { IComment } from '../../classes/Comment';
-import PostComments from './PostComments';
-import { FirestoreHelper } from '../../util/FirestoreHelper';
+import React from 'react'
+import { IPost } from '../../classes/Post'
+import PostMenu from './PostMenu'
+import PostHeader from './PostHeader'
+import PostMedia from './PostMedia'
+import "firebase/firestore"
+import { IHistoryProps } from '../../classes/IHistoryProps'
+import { IComment } from '../../classes/Comment'
+import PostComments from './PostComments'
+import { FirestoreHelper } from '../../util/FirestoreHelper'
 
 interface postDetailsProps {
     post?: IPost
@@ -30,31 +30,54 @@ class PostPage extends React.Component {
 
     public props: postDetailsProps
     public state: postDeatilsState
+    public _mounted: boolean
 
     constructor(props: postDetailsProps) {
         super(props);
         this.props = props;
         this.commentsChanged = this.commentsChanged.bind(this)
+        this.loadPost = this.loadPost.bind(this)
+        this._mounted = false
 
         const needToLoad = (!this.props.post && this.props.match && this.props.match.params && this.props.match.params.postid) as boolean;
         this.state = { post: this.props.post, loading: needToLoad, comments: [] }
 
         if (!this.props.post && this.props.match && this.props.match.params && this.props.match.params.postid) {
-            FirestoreHelper.loadPost(this.props.match.params.postid, (post) => {
-                this.setState({
-                    post: post,
-                    loading: false
-                })
-                FirestoreHelper.loadLocation(post.locationid, (loc) => {
-                    this.setState({ locationname: loc.name })
-                })
-            })
-            FirestoreHelper.loadComments(this.props.match.params.postid, (comments) => {
-                this.setState({ comments: comments })
-            })
+            this.loadPost(this.props.match.params.postid)
         } else {
             console.log("No post id provided")
         }
+    }
+
+    componentDidMount() {
+        this._mounted = true;
+    }
+
+    componentWillUnmount() {
+        this._mounted = false;
+    }
+
+    loadPost(postid: string) {
+        if (this._mounted) {
+            this.setState({
+                loading: true,
+                post: undefined,
+                locationname: undefined,
+                comments: []
+            })
+        }
+        FirestoreHelper.loadPost(postid, (post) => {
+            this.setState({
+                post: post,
+                loading: false
+            })
+            FirestoreHelper.loadLocation(post.locationid, (loc) => {
+                this.setState({ locationname: loc.name })
+            })
+        })
+        FirestoreHelper.loadComments(postid, (comments) => {
+            this.setState({ comments: comments })
+        })
     }
 
     commentsChanged() {
@@ -72,6 +95,12 @@ class PostPage extends React.Component {
             return post.posted.toDate()
         }
         return new Date();
+    }
+
+    componentDidUpdate(prevProps: postDetailsProps, prevState: postDeatilsState) {
+        if (this.props.match && this.props.match.params && this.state.post && this.props.match.params.postid && this.props.match.params.postid !== this.state.post.id) {
+            this.loadPost(this.props.match.params.postid)
+        }
     }
 
     render() {
