18e68fd629dc7d7e1caf627af6b8df651f6427c2
Shawn Goertzen <sgoertzen@gmail.com>
2020-02-23 10:54:01 +0800

    Rendition Manager test fixes

--- functions/src/util/renditionManager.test.tsx
+++ functions/src/util/renditionManager.test.tsx
@@ -3,49 +3,49 @@
  * 
  * @group unit
  */
-import { renditioner } from './renditioner'
+import { renditionManager } from './renditionManager'
 import { equal } from 'assert'
 import { ObjectMetadata } from 'firebase-functions/lib/providers/storage'
 import { videoHelper } from './videoHelper'
 
 it('Validates correct name and type', () => {
     let meta = mockValidMetadata()
-    let videoValid = renditioner.validateVideo(meta)
+    let videoValid = renditionManager.validateVideo(meta)
     equal(videoValid, true)
 })
 
 it('Invalidates missing content types', () => {
     let meta = mockValidMetadata()
     meta.contentType = ""
-    let videoValid = renditioner.validateVideo(meta)
+    let videoValid = renditionManager.validateVideo(meta)
     equal(videoValid, false)
 })
 
 it('Invalidates missing names', () => {
     let meta = mockValidMetadata()
     meta.name = ""
-    let videoValid = renditioner.validateVideo(meta)
+    let videoValid = renditionManager.validateVideo(meta)
     equal(videoValid, false)
 })
 
 it('Invalidates non video file', () => {
     let meta = mockValidMetadata()
     meta.contentType = "image"
-    let videoValid = renditioner.validateVideo(meta)
+    let videoValid = renditionManager.validateVideo(meta)
     equal(videoValid, false)
 })
 
 it('Invalidates non post video', () => {
     let meta = mockValidMetadata()
     meta.name = "movie.avi"
-    let videoValid = renditioner.validateVideo(meta)
+    let videoValid = renditionManager.validateVideo(meta)
     equal(videoValid, false)
 })
 
 it('Invalidates renditions', () => {
     let meta = mockValidMetadata()
     meta.name = `${videoHelper.RENDITION_PREFIX}_file.avi`
-    let videoValid = renditioner.validateVideo(meta)
+    let videoValid = renditionManager.validateVideo(meta)
     equal(videoValid, false)
 })
 
